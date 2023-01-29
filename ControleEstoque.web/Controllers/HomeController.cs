798a9913d9e722cbfeb9e2c7d3c3da8223aef841
danieldossantossouza <danieldossantossouza.88gmail.com>
2023-01-29 15:12:38 -0300

    Criando Authenticação do Usuário

--- ControleEstoque.web/Controllers/HomeController.cs
+++ ControleEstoque.web/Controllers/HomeController.cs
@@ -8,11 +8,13 @@ namespace ControleEstoque.web.Controllers
 {
     public class HomeController : Controller
     {
+       
         public ActionResult Index()
         {
             return View();
         }
 
+        
         public ActionResult Sobre()
         {
             return View();
---
+++ ControleEstoque.web/Models/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControleEstoque.web.Models
+{
+    public class LoginViewModel
+    {
+        [Required (ErrorMessage ="Informe o usuário")]
+        [Display (Name ="Usuário: ")]
+        public string Usuario { get; set; }
+
+        [Required(ErrorMessage = "Informe  senha")]
+        [DataType (DataType.Password)]
+        [Display(Name = "Senha: ")]
+        public string Senha { get; set; }
+
+        [Display(Name = "Lembrar Me: ")]
+        public bool LembrarMe { get; set; }
+    }
+
+}
