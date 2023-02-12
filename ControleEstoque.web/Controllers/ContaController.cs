b11c4248e01600c5105807c6a6257e71e237d9b1
danieldossantossouza <danieldossantossouza.88gmail.com>
2023-02-12 15:02:15 -0300

    Criando ConnectionString do usuario no banco de dados.

--- ControleEstoque.web/Controllers/ContaController.cs
+++ ControleEstoque.web/Controllers/ContaController.cs
@@ -27,7 +27,7 @@ namespace ControleEstoque.web.Controllers
                 return View(login);
             }
 
-            var achou = (login.Usuario == "Daniel" && login.Senha == "123");
+            var achou = UsuarioModel.ValidarUsuario(login.Usuario, login.Senha);
 
             if (achou)
             {
